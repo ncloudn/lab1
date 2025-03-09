2a031d2822d0e3a931e31f44ac23b6a01880c8dd
Nika Nikitina <nikacloudnikitina@gmail.com>
2025-03-09 14:52:11 +0300

    Initial commit: CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int add(int a, int b);
+int subtract(int a, int b);
+
+int main() {
+    int a = 10, b = 5;
+    std::cout << "Сумма: " << add(a, b) << std::endl;
+    std::cout << "Разница: " << subtract(a, b) << std::endl;
+}
